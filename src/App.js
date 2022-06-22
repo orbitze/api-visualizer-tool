fa248346345d844fd2a71eafdc134dc40f3d74d2
orbitze <mverma1427@gmail.com>
2022-06-22 14:50:46 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import DataForm from './components/DataForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <DataForm />
+    </div>
+  );
+}
+
+export default App;
